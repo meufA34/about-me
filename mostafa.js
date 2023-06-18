a32f87d1166cbf7db1c7b744ab236e4c0c38db84
meufA34 <98660033+meufA34@users.noreply.github.com>
2023-06-18 12:33:50 +0300

    Add files via upload

---
+++ mostafa.js
@@ -0,0 +1,17 @@
+
+// Get the button element
+var scrollToTopBtn = document.getElementById("button3");
+
+// Scroll to the top when the button is clicked
+scrollToTopBtn.addEventListener("click", function() {
+  document.body.scrollTop = 0; // For Safari
+ 
+});
+
+
+let marioBTN = document.getElementById("marioButton");
+marioBTN.addEventListener("click", () => {
+  location.href=("https://meufa34.github.io/mario/")
+})
+
+
